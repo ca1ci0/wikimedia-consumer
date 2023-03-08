6626242afe3c8fa0b86ba46e8f975537530c98bc
ca1ci0 <mykolakoval1997@gmail.com>
2023-03-09 00:58:55 +0200

    add project initial structure

---
+++ src/test/java/com/ca1ci0/wikimedia/consumer/WikimediaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ca1ci0.wikimedia.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikimediaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
